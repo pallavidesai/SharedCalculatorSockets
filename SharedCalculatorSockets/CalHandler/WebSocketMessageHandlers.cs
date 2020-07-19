80d87cb7f3f6252756d6219f7bf47781446e715e
Pallavi Desai <s.pallavidesai@gmail.com>
2020-07-18 23:50:09 -0500

    Basic Socket Connection Code

---
+++ SharedCalculatorSockets/CalHandler/WebSocketMessageHandlers.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.WebSockets;
+using System.Text;
+using System.Threading.Tasks;
+using SharedCalculatorSockets.CalSocketManager;
+
+namespace SharedCalculatorSockets.CalHandler
+{
+    public class WebSocketMessageHandlers : CalSocketManager.CalSocketHandler
+    {
+        public WebSocketMessageHandlers(ConnectionManager connections) : base(connections)
+        {
+
+        }
+
+        public override async Task OnConnected(WebSocket socket)
+        {
+            await  base.OnConnected(socket);
+            var socketID = Connections.GetIDOfSocket(socket);
+            await SendMessageToAll($"{socketID} just joined the party**********");
+        }
+
+        public override async Task Receive(WebSocket socket, WebSocketReceiveResult result, byte[] buffer)
+        {
+            var socketID = Connections.GetIDOfSocket(socket);
+            var message = $"{socketID} said- {Encoding.ASCII.GetString(buffer, 0, result.Count)}";
+            Console.WriteLine(message.ToString());
+            await SendMessageToAll (message);
+        }
+    }
+}
---
+++ SharedCalculatorSockets/CalSocketManager/CalSocketExtension.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
+
+namespace SharedCalculatorSockets.CalSocketManager
+{
+    public static class CalSocketExtension
+    {
+        public static IServiceCollection AddWebSocketManager(this IServiceCollection services)
+        {
+            services.AddTransient<ConnectionManager>();
+            foreach (var type in Assembly.GetEntryAssembly().ExportedTypes)
+            {
+                if (type.GetTypeInfo().BaseType == typeof(CalSocketHandler))
+                    services.AddSingleton(type);
+            }
+            return services;
+        }
+
+        public static IApplicationBuilder MapSockets(this IApplicationBuilder app, PathString path, CalSocketHandler socket)
+        {
+            return app.Map(path, (p) => p.UseMiddleware<CalSocketMiddleware>(socket));
+        }
+
+    }
+}
